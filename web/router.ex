bdc6204e6761044d15ee88be97d8796583c87aeb
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-03-13 19:27:40 -0500

    removed guardian funcs

--- web/router.ex
+++ web/router.ex
@@ -5,28 +5,28 @@ defmodule GroceryGnome.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    # plug :protect_from_forgery
+    # plug :put_secure_browser_headers
   end
 
 	# plug looks for a guardian token in session in default location # what is the default location
 	# attempt to load resource found in the jwt
 	# if no jwt in default location, does nothing
-	pipeline :browser_auth do
-		plug Guardian.Plug.Verify
-		plug Guardian.Plug.LoadResource
-	end
+	# pipeline :browser_auth do
+	# 	plug Guardian.Plug.Verify
+	# 	plug Guardian.Plug.LoadResource
+	# end
 	
-	# admin pipeline.
-	pipeline :admin_browser_auth do
-		plug Guardian.Plug.VerifySession, key: :admin
-		plug Guardian.Plug.LoadResource, key: :admin
-	end
+	# # admin pipeline.
+	# pipeline :admin_browser_auth do
+	# 	plug Guardian.Plug.VerifySession, key: :admin
+	# 	plug Guardian.Plug.LoadResource, key: :admin
+	# end
 
-	# impersonation plug
-	pipeline :impersonation_browser_auth do
-		plug Guardian.Plug.VerifySession, key: :admin
-	end
+	# # impersonation plug
+	# pipeline :impersonation_browser_auth do
+	# 	plug Guardian.Plug.VerifySession, key: :admin
+	# end
 
 	
 
@@ -35,16 +35,16 @@ defmodule GroceryGnome.Router do
   end
 
   scope "/", GroceryGnome do
-    pipe_through [:browser, :browser_auth, :impersonation_browser_auth]
+    pipe_through [:browser]
 
     get "/", PageController, :index
 		delete "/logout", AuthController, :logout
 
-		resources "/users", UserController
-		resources "/authorizations", AuthorizationController
-		resources "/tokens", TokenController
+		# resources "/users", UserController
+		# resources "/authorizations", AuthorizationController
+		# resources "/tokens", TokenController
 
-		get "/private", PrivatePageController, :index
+		# get "/private", PrivatePageController, :index
   end
 
   # Other scopes may use custom stacks.
