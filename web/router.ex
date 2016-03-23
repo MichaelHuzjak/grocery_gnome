f37619d9db600a47b2369695bfa304ed40773265
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-03-22 20:13:58 -0500

    remove some cruft

--- web/router.ex
+++ web/router.ex
@@ -9,27 +9,6 @@ defmodule GroceryGnome.Router do
     # plug :put_secure_browser_headers
   end
 
-	# plug looks for a guardian token in session in default location # what is the default location
-	# attempt to load resource found in the jwt
-	# if no jwt in default location, does nothing
-	# pipeline :browser_auth do
-	# 	plug Guardian.Plug.Verify
-	# 	plug Guardian.Plug.LoadResource
-	# end
-	
-	# # admin pipeline.
-	# pipeline :admin_browser_auth do
-	# 	plug Guardian.Plug.VerifySession, key: :admin
-	# 	plug Guardian.Plug.LoadResource, key: :admin
-	# end
-
-	# # impersonation plug
-	# pipeline :impersonation_browser_auth do
-	# 	plug Guardian.Plug.VerifySession, key: :admin
-	# end
-
-	
-
   pipeline :api do
     plug :accepts, ["json"]
   end
