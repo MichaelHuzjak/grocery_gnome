5497de57becb076ae791236436cfc31984bc69c3
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-03-22 20:26:55 -0500

    index page cleared

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule GroceryGnome.PageController do
   use GroceryGnome.Web, :controller
 
   def index(conn, _params) do
-    render conn, "welcome.html"
+    render conn, "index.html"
   end
 
 end
