264a6c445fd59a5644f6fed13ea71d929c657497
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-03-22 19:48:51 -0500

    more

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,8 +4,10 @@ defmodule GroceryGnome.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
-
-	def test(conn, _params) do
+	def test(conn, _params) do 
 		render conn, "test.html"
+	end
+	def splash(conn, _params) do
+		render conn, "spash.html"
 	end
 end
--- web/router.ex
+++ web/router.ex
@@ -2,33 +2,12 @@ defmodule GroceryGnome.Router do
   use GroceryGnome.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
+		plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
     # plug :protect_from_forgery
     # plug :put_secure_browser_headers
   end
-
-	# plug looks for a guardian token in session in default location # what is the default location
-	# attempt to load resource found in the jwt
-	# if no jwt in default location, does nothing
-	# pipeline :browser_auth do
-	# 	plug Guardian.Plug.Verify
-	# 	plug Guardian.Plug.LoadResource
-	# end
-	
-	# # admin pipeline.
-	# pipeline :admin_browser_auth do
-	# 	plug Guardian.Plug.VerifySession, key: :admin
-	# 	plug Guardian.Plug.LoadResource, key: :admin
-	# end
-
-	# # impersonation plug
-	# pipeline :impersonation_browser_auth do
-	# 	plug Guardian.Plug.VerifySession, key: :admin
-	# end
-
-	
 
   pipeline :api do
     plug :accepts, ["json"]
@@ -37,16 +16,10 @@ defmodule GroceryGnome.Router do
   scope "/", GroceryGnome do
     pipe_through [:browser]
 
-    get "/", PageController, :index
+		get "/", PageController, :index
 		delete "/logout", AuthController, :logout
 		get "/", PageController, :splash
 		get "/test", PageController, :test
-
-		# resources "/users", UserController
-		# resources "/authorizations", AuthorizationController
-		# resources "/tokens", TokenController
-
-		# get "/private", PrivatePageController, :index
   end
 
   # Other scopes may use custom stacks.
