6e8e45fd113212fd84f62bf7391555fee9d19d5a
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-04-04 17:43:59 -0500

    .

--- web/controllers/pantry_controller.ex
+++ web/controllers/pantry_controller.ex
@@ -1,7 +1,18 @@
 defmodule GroceryGnome.PantryController do
 	use GroceryGnome.Web, :controller
 
+	plug GroceryGnome.Plug.Authenticate
+	plug :action
+	alias GroceryGnome.Fooditem		# this will have to whatever model is used for pantry
+	import Ecto.Query
+
 	def index(conn, _params) do
+		userid = conn.assigns.current_user.id
+		query = from f in Fooditems,
+		where: f.user_id == ^userid,
+		select: f
+		
+		items = Repo.all()
 		render conn, "index.html"
 	end
 end
