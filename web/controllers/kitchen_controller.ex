b7dde2a640fd2946fa4c3608bedb6825e113a8a8
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-04-04 22:35:39 -0500

    formatting

--- web/controllers/kitchen_controller.ex
+++ web/controllers/kitchen_controller.ex
@@ -2,6 +2,6 @@ defmodule GroceryGnome.KitchenController do
 	use GroceryGnome.Web, :controller
 
 	def index(conn, _params) do
-		render conn, "kitchen.html"
+		render(conn, "kitchen.html")
 	end
 end
