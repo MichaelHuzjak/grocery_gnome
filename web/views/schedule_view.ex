a44a6f31e6db15534390f330c76e7fa9f0ead04a
Jacob Sonnenberg <jacob@galactica.lan.us>
2016-04-23 21:44:38 -0500

    summarize recipes

--- web/views/schedule_view.ex
+++ web/views/schedule_view.ex
@@ -1,3 +1,14 @@
 defmodule GroceryGnome.ScheduleView do
   use GroceryGnome.Web, :view
+
+	# terrible
+	def fetch(id) do
+		try do
+			{:ok, resp} = GroceryGnome.Spoonacular.summarize_recipe(id)
+			resp["title"]
+		rescue
+			HTTPotion.HTTPError -> ""
+		end
+	end
+
 end
