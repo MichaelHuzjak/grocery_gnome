81f9b1de498e399fb8535ddbbf37167758c2da3f
Jacob Sonnenberg <jacob@galactica.lan.us>
2016-04-25 20:33:11 -0500

    err

--- web/controllers/schedule_controller.ex
+++ web/controllers/schedule_controller.ex
@@ -13,14 +13,17 @@ defmodule GroceryGnome.ScheduleController do
 	end
 
 	def generate(conn, _params) do
+		date = conn.params["form_data"]["date"]
 		userid = conn.assigns.current_user.id
 		{:ok, meals} = GroceryGnome.Spoonacular.caloried_meal_plan 2000, "day"
 		[breakfast, lunch, dinner] = meals["meals"]
+		IO.inspect date
+		d = ~s/#{date["year"]}-#{date["month"]}-#{date["day"]}/
 		changeset = %Day{
 			breakfast: [breakfast["id"], 1433],
 			lunch: [lunch["id"], 1234, 51515],
 			dinner: [dinner["id"]],
-			date: to_string(Ecto.Date.utc),
+			date: d,
 			user_id: userid,
 		}
 		# render(conn, "index.html", changeset: changeset)
@@ -35,6 +38,21 @@ defmodule GroceryGnome.ScheduleController do
 				|> put_flash(:error, "Error occured")
 				|> redirect(to: schedule_path(conn, :index))
 		end
+	end
+
+	def delete(conn, %{id: id}) do
+		day = Repo.get!(Day, id)
+
+		Repo.delete!(day)
+
+		conn
+		|> put_flash(:info, "Deleted day")
+		|> redirect(to: schedule_path(conn, :index))
+	end
+
+	def new(conn, _params) do
+		changeset = Day.changeset(%Day{})
+		render(conn, "new.html", changeset: changeset)
 	end
 
 end
