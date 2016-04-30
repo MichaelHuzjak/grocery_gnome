deaaf74b2710c284a87c7f57a339162ada5ac3f8
Diana <diana@grocerygnome.grocerygnome>
2016-04-29 20:42:28 -0500

    hey guys! I got that notification sh*t working! PS... there are only 30 days in April! LOLZ

--- web/views/shared_view.ex
+++ web/views/shared_view.ex
@@ -1,3 +1,12 @@
 defmodule GroceryGnome.SharedView do
 	use GroceryGnome.Web, :view
+	alias GroceryGnome.Pantryitem
+	alias GroceryGnome.Foodcatalog.Foodcatalog
+  import Ecto.Query
+	import Ecto.Date
+	import Ecto.Repo
+	
+	def notifyExpiration(conn) do
+		pantryitems = GroceryGnome.PageController.expiration_notifications conn
+	end
 end
