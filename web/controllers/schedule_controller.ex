4873898f5bf2e9ed8803dd0ec240813695b56a22
Jacob Sonnenberg <jacobsonnenberg0@gmail.com>
2016-04-30 15:57:58 -0500

    fix the merge break

--- web/controllers/schedule_controller.ex
+++ web/controllers/schedule_controller.ex
@@ -113,7 +113,6 @@ defmodule GroceryGnome.ScheduleController do
 					user_id: userid})
 		
 		case Repo.insert(changeset) do
-<<<<<<< HEAD
 			{:ok, day} ->
 				for {type, mls} <- [{0, breakfast}, {1, lunch}, {2, dinner}] do
 					{:ok, m} = GroceryGnome.Spoonacular.recipe_information mls["id"], false
@@ -127,47 +126,6 @@ defmodule GroceryGnome.ScheduleController do
 					Repo.insert(meal)
 				end
 					
-=======
-			{:ok, _day} ->
-				#Generate the Meals
-						#breakfast
-			{:ok, b} = GroceryGnome.Spoonacular.recipe_information breakfast["id"], false
-				IO.inspect b
-				recipe_add(userid,b)
-				b = Repo.get_by(Recipe, recipe_title: b["title"])
-				meal = Meal.changeset(%Meal{}, %{
-							meal_type: 0,
-							recipe_id: b.id,
-							day_id: _day.id,
-																	 })
-				Repo.insert(meal)
-				#lunch
-				{:ok, l} = GroceryGnome.Spoonacular.recipe_information lunch["id"], false
-				IO.inspect l
-				recipe_add(userid,l)
-				l = Repo.get_by(Recipe, recipe_title: l["title"])
-				meal = Meal.changeset(%Meal{}, %{
-							meal_type: 1,
-							recipe_id: l.id,
-							day_id: _day.id,
-																	 })
-				Repo.insert(meal)
-
-				#dinner
-				{:ok, d} = GroceryGnome.Spoonacular.recipe_information dinner["id"], false
-				IO.inspect d
-				recipe_add(userid,d)
-				d = Repo.get_by(Recipe, recipe_title: d["title"])
-				meal = Meal.changeset(%Meal{}, %{
-							meal_type: 2,
-							recipe_id: d.id,
-							day_id: _day.id,
-																	 })
-								Repo.insert(meal)
-
-								# ensure
-								ensure_schedule(userid)
->>>>>>> ensure
 				conn
 				|> put_flash(:info, "Generated plan")
 				|> redirect(to: schedule_path(conn, :index))
